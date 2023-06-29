6f5b618aec1a97a4cadad5633d715470af7401c6
Minsoo-Lee <minsoo.james1101@gmail.com>
2023-06-29 23:41:09 +0900

    #3 8 ID로 특정 게시물 지우기

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,5 +1,5 @@
 import { BoardsService } from './boards.service';
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { Board } from './board.model';
 import { CreateBoardDto } from './dto/create-board.dto';
 
@@ -22,5 +22,10 @@ export class BoardsController {
     @Get('/:id')
     getBoardById(@Param('id') id: string): Board {
         return this.boardsService.getBoardById(id);
+    }
+
+    @Delete('/:id')
+    deleteBoard(@Param('id') id: string) : void {
+        this.boardsService.deleteBoard(id);
     }
 }
