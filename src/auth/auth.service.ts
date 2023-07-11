e75581f618909bd130a4106811cd0feb84cb3ce2
Minsoo-Lee <minsoo.james1101@gmail.com>
2023-07-11 14:29:31 +0900

    #7 7 로그인 기능 구현하기

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
 import { AuthCredentialDto } from './dto/auth-credential';
 import { UserRepository } from './user.repository';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
@@ -12,5 +13,16 @@ export class AuthService {
 
     async signUp(authCredentialDto: AuthCredentialDto): Promise<void> {
         return this.userRepository.createUser(authCredentialDto);
+    }
+
+    async signIn(authCredentialDto: AuthCredentialDto): Promise<string> {
+        const { username, password } = authCredentialDto;
+        const user = await this.userRepository.findOne({ username });
+
+        if (user && (await bcrypt.compare(password, user.password))) {
+            return 'login success';
+        } else {
+            throw new UnauthorizedException('login fail');
+        }
     }
 }
