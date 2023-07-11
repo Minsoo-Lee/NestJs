e6dcdc2c4ed83e5133190315e5d6e212bbff49c4
Minsoo-Lee <minsoo.james1101@gmail.com>
2023-07-11 14:51:17 +0900

    #7 5 JWT를 이용해서 토큰 생성하기

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,12 +3,21 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([UserRepository])
-  ],
-  controllers: [AuthController],
-  providers: [AuthService]
+	imports: [
+		PassportModule.register({ defaultStrategy: 'jwt' }),
+		JwtModule.register({
+			secret: 'Secret1234',
+			signOptions: {
+				expiresIn: 3600
+			}
+		}),
+		TypeOrmModule.forFeature([UserRepository])
+	],
+	controllers: [AuthController],
+	providers: [AuthService]
 })
 export class AuthModule {}
