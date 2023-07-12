101e30783717271eded62ae6e68c7866e59ace72
Minsoo-Lee <minsoo.james1101@gmail.com>
2023-07-13 03:40:31 +0900

    Introduction - Controller

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Header, HttpCode, Post, Req } from '@nestjs/common';
+import { Controller, Get, Header, HttpCode, Param, Post, Query, Redirect, Req } from '@nestjs/common';
 
 @Controller('cats')
 export class CatsController {
@@ -10,7 +10,21 @@ export class CatsController {
     }
 
     @Get()
+    @Redirect('https://nestjs.com', 301)
     findAll(@Req() request: Request): string {
         return 'This action returns all cats';
+    }
+
+    @Get('docs')
+    @Redirect('https://docs.nestjs.com', 302)
+    getDocs(@Query('version') version) {
+        if (version && version === '5')
+            return { url: 'https://docs.nestjs.com/v5/'};
+    }
+
+    @Get(':id')
+    findOne(@Param() params: any): string {
+        console.log(params.id);
+        return `This action returns a #${params.id} cat`;
     }
 }
